2f59af3f142365d182c81b2f4a4e2f54f246a181
Richard Shephard <richards@sahomeloans.com>
2019-09-06 13:24:18 +0200

    Added tailwind config

--- responsive-sidebar-1/postcss.config.js
+++ responsive-sidebar-1/postcss.config.js
@@ -1,5 +1,17 @@
+const tailwindcss = require('tailwindcss')
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./public/**/*.html'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss, cssnano]
+      : []
+  ]
 }

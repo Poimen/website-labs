a06edc0a084302c5548e785427399a4cad6f0966
Richard Shephard <richards@sahomeloans.com>
2019-09-29 21:08:59 +0200

    Add navbar component

---
+++ reactjs/responsive-navbar-1/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+export function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="bg-green-800">
+      <div className="flex items-center justify-between h-24">
+        <div>
+          <img className="h-12 w-12" src="logo192.png" alt="logo" />
+        </div>
+        <div className="sm:hidden">
+          <button onClick={() => setIsOpen(!isOpen)} type="button" className="block text-gray-500 hover:text-gray-900 focus:outline-none">
+            <svg className="h-12 w-12 fill-current" viewBox="0 0 24 24">
+              {isOpen  ? <path fillRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" /> : null}
+              {!isOpen ? <path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" /> : null}
+            </svg>
+          </button>
+        </div>
+      </div>
+        <div className={`sm:flex sm:p-0 sm:block ${isOpen ? 'block' : 'hidden'}`}>
+          <a href="#" className="block px-2 py-2 text-white font-semibold rounded hover:bg-gray-600">Dashboard</a>
+          <a href="#" className="block px-2 py-2 text-white font-semibold rounded hover:bg-gray-600">Contact Us</a>
+        </div>
+    </header>
+    )
+}
